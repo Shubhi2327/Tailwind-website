2ae7c5ae479cb70537b2e4f176e84e5a18e5fcd2
Shubhi Swami <shubhiswami845@gmail.com>
2024-07-13 12:50:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary' :'#ee0181'
+        'shubhi' : '#01ee91'
+      },
+      fontFamily: {
+        'shubhi-font':['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
